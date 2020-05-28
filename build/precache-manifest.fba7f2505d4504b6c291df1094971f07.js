f9a5c028991f9322383055cd2385c927c0b15a4c
Vishal <geek4teck@gmail.com>
2020-05-28 17:29:16 +0530

    First version..

---
+++ build/precache-manifest.fba7f2505d4504b6c291df1094971f07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba7555d142818e87f9dd021bec3bca87",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3f9542f2740aaa1c8fc8",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1d912ee3d9cba5d8d203",
+    "url": "./static/js/2.1a4d3279.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.1a4d3279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f9542f2740aaa1c8fc8",
+    "url": "./static/js/main.b45a4061.chunk.js"
+  },
+  {
+    "revision": "87e1e2b009f1ec8614d0",
+    "url": "./static/js/runtime-main.f6233dd2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ build/preload.js
@@ -0,0 +1,1 @@
+const {app,electron} = require('electron').remote;
